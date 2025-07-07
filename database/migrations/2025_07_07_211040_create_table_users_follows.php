c5ee33d4c5a114887de0a5a7771f047264f883df
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-07-07 18:56:30 -0300

    feat: create users_follows table

---
+++ database/migrations/2025_07_07_211040_create_table_users_follows.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_follows', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_following');
+            $table->unsignedBigInteger('user_followed');
+            $table->primary(['user_following', 'user_followed']);
+            $table->foreign('user_following')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_followed')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('followed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_follows');
+    }
+};
