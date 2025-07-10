5e93ab16aae741f7f41921ec15e3933841538d0b
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-07-10 00:46:43 -0300

    feat: adding routes of user controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::patch('/profile', [UserController::class, 'update'])->name('profile.update');
 });
