ca16e9c603a20109a2886bc61f686abc6599e886
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-07-17 17:19:38 -0300

    feat: adding routes to return forms

--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,36 @@ use App\Http\Controllers\UserFollowController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
+// auth routes
 Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
+// view post
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+// view user profile
+Route::get('/profile/{userId}', [UserController::class, 'show'])->name('profile.show');
+
 Route::middleware('auth')->group(function () {
     //user routes
     Route::patch('/profile', [UserController::class, 'update'])->name('profile.update');
+    Route::get('/profile/{userId}/edit', [UserController::class, 'edit'])->name('profile.edit');
 
     //post routes
-    Route::post('/posts', [PostController::class, 'store'])->name('posts.create');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.delete');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
     //comment routes
-    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.create');
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::patch('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
-    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.delete');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 
     //follow routes
     Route::post('/follow/{userId}', [UserFollowController::class, 'follow'])->name('follow');
