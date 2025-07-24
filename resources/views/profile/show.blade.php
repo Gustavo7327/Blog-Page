2fe67766ee1a0bbd8ac575d714d6d4c87f464b97
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-07-24 16:46:36 -0300

    feat: icons for follow and unfollow actions

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -32,7 +32,20 @@
                     @endif
                     <div class="flex flex-wrap gap-4 mt-4">
                         <div class="bg-gray-700 px-4 py-2 rounded text-gray-200 text-sm flex items-center gap-2">
-                            <svg class="w-5 h-5 text-blue-400" viewBox="0 0 24 24" fill="currentColor"><path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13Z"></path></svg>
+                            @if(auth()->check() && auth()->user()->id !== $user->id && auth()->user()->follows()->where('user_followed', $user->id)->exists())
+                                <form action="{{ route('unfollow', $user->id) }}" method="POST" class="flex items-center">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b82f6" class="w-5 h-5"><path d="M13 14.0619V22H4C4 17.5817 7.58172 14 12 14C12.3387 14 12.6724 14.021 13 14.0619ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM17.7929 19.9142L21.3284 16.3787L22.7426 17.7929L17.7929 22.7426L14.2574 19.2071L15.6716 17.7929L17.7929 19.9142Z"></path></svg></button>
+                                </form>
+                            @elseif(auth()->check() && auth()->user()->id !== $user->id && !auth()->user()->follows()->where('user_followed', $user->id)->exists())
+                                <form action="{{ route('follow', $user->id) }}" method="POST" class="flex items-center">
+                                    @csrf
+                                    <button type="submit" class=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b82f6" class="w-5 h-5"><path d="M14 14.252V22H4C4 17.5817 7.58172 14 12 14C12.6906 14 13.3608 14.0875 14 14.252ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM19 16.5858L21.1213 14.4645L22.5355 15.8787L20.4142 18L22.5355 20.1213L21.1213 21.5355L19 19.4142L16.8787 21.5355L15.4645 20.1213L17.5858 18L15.4645 15.8787L16.8787 14.4645L19 16.5858Z"></path></svg></button>
+                                </form>
+                            @else
+                                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="#3b82f6"><path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13Z"></path></svg>
+                            @endif
                             <span>{{ $user->followers()->count() }} Followers</span>
                         </div>
                         <div class="bg-gray-700 px-4 py-2 rounded text-gray-200 text-sm flex items-center gap-2">
